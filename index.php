0c78db6c3385d154c8ca5c95156323f0c0625e2f
joanamaia03 <93883152+joanamaia03@users.noreply.github.com>
2023-05-16 21:01:37 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<link rel="stylesheet" href="main.css">
+<!DOCTYPE html>
+<html lang = "en-US">
+    <head>
+	    <title>Loving and Learning: Your Guide to Healthy, Happy Relationships</title>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+	   <header>
+            <img src="Logo.png" alt="Brand" class="brand">
+            <input type="text" placeholder="Search...">
+            <a href="login.php" class="log_reg">Login</a>
+            <a href="register.php" class="log_reg">Register</a>
+        </header>
+        <nav class="menu">
+            <ul>
+				<li><a href="index.php">Initial page</a></li>
+                <li><a href="tips.php">Tips</a></li>
+                <li><a href="questions.php">Questions</a></li>
+				<li><a href="perfil.php">Profile</a></li>
+            </ul>
+          </nav>
+	    <main>
+		    <section class="banner">
+			    <h2>What do you want to know about love?</h2>
+			    <p>Here you will find tips and answers to your love questions!</p>
+		    </section>
+
+		    <section class="tips">
+			    <h2>Featured tips</h2>
+			    <ul>
+				    <li>
+					 
+					    <h3>Tip 1</h3>
+					    <p>Descrição da Dica 1</p>
+				    </li>
+				    <li>
+			
+					    <h3>Tip 2</h3>
+					    <p>Descrição da Dica 2</p>
+				    </li>
+				    <li>
+					
+					    <h3>Tip 3</h3>
+					    <p>Descrição da Dica 3</p>
+				    </li>
+			    </ul>
+		    </section>
+
+		    <section class="questions">
+			    <h2>Recent questions</h2>
+			    <ul>
+				    <li>
+					    <h3>Título da Pergunta 1</h3>
+					    <p>Descrição da Pergunta 1</p>
+					    <a href="#">Leia Mais</a>
+				    </li>
+				    <li>
+					    <h3>Título da Pergunta 2</h3>
+					    <p>Descrição da Pergunta 2</p>
+					    <a href="#">Leia Mais</a>
+				    </li>
+				    <li>
+					    <h3>Título da Pergunta 3</h3>
+					    <p>Descrição da Pergunta 3</p>
+					    <a href="#">Leia Mais</a>
+				    </li>
+			    </ul>
+		    </section>
+	    </main>
+
+	    <footer>
+		    <p>&copy;Love and Learning, 2023</p>
+	    </footer>
+    </body>
+</html>
