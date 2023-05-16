04ac6f29106901320eb9791a89f94ff39b9cb68e
joanamaia03 <93883152+joanamaia03@users.noreply.github.com>
2023-05-16 18:45:22 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,87 @@
+<link rel="stylesheet" href="register.css">
+<link rel="stylesheet" href="main.css">
+<!DOCTYPE html>
+<html lang = "en-US">
+    <head>
+        <title> Ticket line </title>
+        <meta charset="UTF-8"> 
+    </head>
+    <body>
+        <header>
+            <img src="Group 1 (1).png" alt="Brand" class="brand">
+            <input type="text" placeholder="Search...">
+            <a href="login.php" class="log_reg">Login</a>
+            <a href="register.php" class="log_reg">Register</a>
+        </header>
+        <nav class="menu">
+            <ul>
+                <li><a href="index.php">Initial page</a></li>
+                <li><a href="tips.php">Tips</a></li>
+                <li><a href="questions.php">Questions</a></li>
+                <li><a href="perfil.php">Profile</a></li>
+            </ul>
+          </nav>
+        <div class="frase">
+            <p>Create your Magic Ticket account</p>
+        </div>
+        <section id="register">
+            <form>
+                <label>
+                Username 
+                </label>
+                <input type="username" name="username"><br></br>
+                <label>
+                E-mail 
+                </label>
+                <input type="email" name="email"><br></br>
+                <label>
+                Password 
+                </label>
+                <input type="password" name="password"><br></br>
+            </form>
+            <div class="button">
+                <button formaction="#" formmethod="post">Register</button>
+                <p class="center">Already have an account?<a href="login.php" class="login">Login</a></p>
+            </div>
+        </section>
+        <footer>
+            <p>&copy;Love and Learning, 2023</p>
+        </footer>
+    </body>
+</html> 
+
+<?php
+/*
+  // Retrieve form data
+  $name = $_POST['name'];
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Validate username
+  if (empty($username)) {
+    $_SESSION['error'] = "Please enter a username.";
+    header('Location: index.php');
+    exit();
+  }
+
+  // Validate password
+  if (empty($password)) {
+    $_SESSION['error'] = "Please enter a password.";
+    header('Location: index.php');
+    exit();
+  }
+
+  // Insert user data into database
+  $sql = "INSERT INTO users (name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";
+  // Execute SQL statement
+  // ...
+
+  // Set the user as logged in
+  $_SESSION['username'] = $username;
+
+
+  // Redirect user to login page
+  header('Location: login.php');
+  */
+?>
---
+++ tips.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="main.css">
+<!DOCTYPE html>
+<html lang = "en-US">
+    <head>
+	    <title>Love and Learning: Your Guide to Healthy, Happy Relationships</title>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+	<header>
+            <img src="Group 1 (1).png" alt="Brand" class="brand">
+            <input type="text" placeholder="Search...">
+            <a href="login.php" class="log_reg">Login</a>
+            <a href="register.php" class="log_reg">Register</a>
+        </header>
+        <nav class="menu">
+            <ul>
+                <li><a href="index.php">Initial page</a></li>
+                <li><a href="tips.php">Tips</a></li>
+                <li><a href="questions.php">Questions</a></li>
+                <li><a href="perfil.php">Profile</a></li>
+            </ul>
+        </nav>
+
+	    <main>
+		    <section class="banner">
+			    <h2>What do you want to know about love?</h2>
+			    <p>Here you will find tips and answers to your love questions!</p>
+			    <a href="#" class="button">Start now</a>
+		    </section>
+
+		    <section class="featured">
+			    <h2>Featured tips</h2>
+			    <ul>
+				    <li>
+					    <img src="imagem1.jpg" alt="Imagem 1">
+					    <h3>Tip 1</h3>
+					    <p>Descrição da Dica 1</p>
+				    </li>
+				    <li>
+					    <img src="imagem2.jpg" alt="Imagem 2">
+					    <h3>Tip 2</h3>
+					    <p>Descrição da Dica 2</p>
+				    </li>
+				    <li>
+					    <img src="imagem3.jpg" alt="Imagem 3">
+					    <h3>Tip 3</h3>
+					    <p>Descrição da Dica 3</p>
+				    </li>
+			    </ul>
+		    </section>
+	    </main>
+
+	    <footer>
+		    <p>© Love and Learning: Your Guide to Healthy, Happy Relationships 2023</p>
+	    </footer>
+    </body>
+</html>
