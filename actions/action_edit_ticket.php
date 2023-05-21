698648b97d711a3f2eb55e312930b5e0a8fbbe2a
joanamaia03 <93883152+joanamaia03@users.noreply.github.com>
2023-05-21 23:52:40 +0100

    Add files via upload

---
+++ actions/action_edit_ticket.php
@@ -0,0 +1,34 @@
+<?php
+    declare(strict_types = 1);
+
+    require_once(__DIR__ . '/../utils/session.php');
+    $session = new Session();
+    
+    if (!$session->isLoggedIn()) {
+        $session->addMessage('error', 'You must be logged in to perform this action');
+        die(header('Location: /'));
+    }
+    
+    require_once(__DIR__ . '/../database/connection.db.php');
+    require_once(__DIR__ . '/../database/question.class.php');
+    
+    if (trim($_POST['title']) === '') {
+        $session->addMessage('error', 'Question title cannot be empty');
+        die(header('Location: ' . $_SERVER['HTTP_REFERER']));
+    }
+    
+    $db = getDatabaseConnection();
+    
+    $question = Question::getQuestion($db, intval($_POST['id']));
+    
+    if ($question) {
+        $question->title = $_POST['title'];
+        $question->save($db);
+        $session->addMessage('success', 'Question title updated');
+        header('Location: ../pages/question.php?id=' . $_POST['id']);
+    } else {
+        $session->addMessage('error', 'Question does not exist');
+        header('Location: ' . $_SERVER);
+    }
+    
+?>
---
+++ actions/action_register.php
@@ -0,0 +1,27 @@
+<?php
+
+    declare(strict_types = 1);
+
+    require_once(__DIR__ . '/../utils/session.php');
+    $session = new Session();
+
+    require_once(__DIR__ . '/../database/connection.db.php');
+    require_once(__DIR__ . '/../database/user.class.php');
+
+    $db = getDatabaseConnection();
+
+    $user = User::createUserWithPassword($db, $_POST['name'], $_POST['username'], $_POST['email'], $_POST['password']);
+
+    if ($user) {
+        $session->setId($user->id);
+        $session->setName($user->name);
+        $session->addMessage('sucess', 'Register successful!');
+        header('Location: ../pages/index.php');
+        
+    }
+    else {
+        $session->addMessage('error', 'Wrong credentials.');
+        header('Location: ../pages/register.php');
+    }
+
+?>
